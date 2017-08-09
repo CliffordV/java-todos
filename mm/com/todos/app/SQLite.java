16b6a837b231ff5a89094d777a96eb21e0b49dbe
clydeatuic <cbalamanatuic@gmail.com>
2017-08-09 12:29:11 +0800

    added assignment guide

---
+++ mm/com/todos/app/SQLite.java
@@ -0,0 +1,120 @@
+package com.todos.app;
+
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+
+public class SQLite {
+    //Class Variables
+    static java.sql.Connection conn  = null;
+    static java.sql.Statement stmt = null;
+    static String url = "jdbc:sqlite:C:\\Users\\Admin01\\Documents\\NetBeansProjects\\EN\\080817_Balaman\\src\\com\\todos\\app\\todos.sqlite";
+    static String error = "";
+    //openDB
+    public static boolean openDB(){
+        boolean result = false;
+        try{
+            Class.forName("org.sqlite.JDBC");
+            conn = java.sql.DriverManager.getConnection(url);
+
+            System.out.println("OK! SQLite DB session connected.");
+            result = true;
+        }
+        catch(Exception e){
+            error = e.getMessage();
+            System.out.println("Open DB Error:" + e.getMessage());
+        } 
+        return result;
+    }    
+    //closeDB
+    public static boolean closeDB(){
+        boolean result = false;
+        try{
+            conn.close();
+
+            System.out.println("OK! SQLite DB session closed.");
+            result = true;
+        }
+        catch(Exception e){
+            error = e.getMessage();
+            System.out.println("Close DB Error: " + e.getMessage());
+        }
+        return result;
+    }    
+    //create
+    public static boolean create(String table, String values){
+        boolean result = false;
+        try{
+            SQLite.stmt = conn.createStatement();
+            String query = "INSERT INTO "+ table +" VALUES(" + values + ")";
+            stmt.executeUpdate(query);
+            //You can include exception handling here. (e.g. Duplicate Data, etc.)
+            result = true;
+        }
+        catch(Exception e){
+            System.out.println("Create Error: " + e.getMessage());
+        }
+        return result;
+    }    
+    //read
+    public static String[][] read(String table){
+        String[][] records = null;
+        try{
+            SQLite.stmt = conn.createStatement();
+
+            //Count total rows
+            ResultSet rs = stmt.executeQuery("SELECT count(*) FROM " + table);
+            int totalRows = rs.getInt(1);
+
+            //Count total columns
+            rs = stmt.executeQuery("SELECT * FROM " + table);
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int totalColumns = rsmd.getColumnCount();
+
+            //Initialize 2D Array "records" with totalRows by totalColumns
+            records = new String[totalRows][totalColumns];
+
+            //Retrieve the record and store it to 2D Array "records"
+            int row=0;
+            while(rs.next()){                
+                for(int col=0,index=1;col<totalColumns;col++,index++){
+                    records[row][col] = rs.getObject(index).toString();
+                }
+                row++;
+            }            
+        }
+        catch(Exception e){
+            System.out.println("Read Error: " + e.getMessage());
+        }
+        return records;
+    }    
+    //update
+    public static boolean update(String table, String set, int id){
+        boolean result = false;
+        try{
+            SQLite.stmt = conn.createStatement();
+            String query = "UPDATE "+ table +" SET " + set + " WHERE id=" + id;
+            stmt.executeUpdate(query);
+            //You can include exception handling here. (e.g. Duplicate Data, etc.)
+            result = true;
+        }
+        catch(Exception e){
+            System.out.println("Create Error: " + e.getMessage());
+        }
+        return result;
+    }    
+    //delete
+    public static boolean delete(String table, int id){
+        boolean result = false;
+        try{
+            SQLite.stmt = conn.createStatement();
+            String query = "DELETE FROM "+ table + " WHERE id=" + id;
+            stmt.executeUpdate(query);
+            result = true;
+        }
+        catch(Exception e){
+            System.out.println("Create Error: " + e.getMessage());
+        }
+        return result;
+    }    
+
+}
